0e9935f0293c6778d0698489ab8c3160cf667dc3
Hugo Borrego <hugborang@alum.us.es>
2025-03-13 13:17:24 +0100

    upgraded Student1 entities

---
+++ src/main/java/acme/entities/flight/FlightRepository.java
@@ -0,0 +1,29 @@
+
+package acme.entities.flight;
+
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface FlightRepository extends AbstractRepository {
+
+	@Query("SELECT min(l.scheduledDeparture) from Leg l where l.flight.id = :flightId")
+	Date findScheduledDeparture(int flightId);
+
+	@Query("SELECT max(l.scheduledArrival) from Leg l where l.flight.id = :flightId")
+	Date findScheduledArrival(int flightId);
+
+	@Query("SELECT l.departureAirport.city from Leg l where l.flight.id = :flightId and l.scheduledDeparture = :departure")
+	String findOriginCity(int flightId, Date departure);
+
+	@Query("SELECT l.arrivalAirport.city from Leg l where l.flight.id = :flightId and l.scheduledArrival = :arrival")
+	String findDestinationCity(int flightId, Date arrival);
+
+	@Query("SELECT COUNT(l) from Leg l where l.flight.id = :flightId")
+	int findNumberOfLegs(int flightId);
+
+}
