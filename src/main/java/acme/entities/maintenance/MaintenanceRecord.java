581eea73993fb2821de9a0ec62e1a4e2423f9f60
jaimegoomez <jaigommar1@alum.us.es>
2025-05-26 20:48:52 +0200

    Indices de task, maintenance record y airport

--- src/main/java/acme/entities/maintenance/MaintenanceRecord.java
+++ src/main/java/acme/entities/maintenance/MaintenanceRecord.java
@@ -7,7 +7,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -29,6 +31,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "draftMode"), @Index(columnList = "ticker")
+})
 public class MaintenanceRecord extends AbstractEntity {
 
 	// Serialisation identifier
