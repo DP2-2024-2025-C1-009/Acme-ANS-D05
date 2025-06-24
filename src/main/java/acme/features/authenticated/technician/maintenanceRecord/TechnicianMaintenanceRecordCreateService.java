9409884eb3d9938a91cf011fd80b98f016026157
jaimegoomez <jaigommar1@alum.us.es>
2025-06-24 18:44:33 +0200

    intento de hacking crear MR arreglado

--- src/main/java/acme/features/authenticated/technician/maintenanceRecord/TechnicianMaintenanceRecordCreateService.java
+++ src/main/java/acme/features/authenticated/technician/maintenanceRecord/TechnicianMaintenanceRecordCreateService.java
@@ -29,6 +29,7 @@ public class TechnicianMaintenanceRecordCreateService extends AbstractGuiService
 		boolean status;
 		String method;
 		int aircraftId;
+		Aircraft aircraft;
 
 		method = super.getRequest().getMethod();
 
@@ -36,7 +37,8 @@ public class TechnicianMaintenanceRecordCreateService extends AbstractGuiService
 			status = true;
 		else {
 			aircraftId = super.getRequest().getData("aircraft", int.class);
-			status = aircraftId == 0 || super.getRequest().getPrincipal().hasRealmOfType(Technician.class);
+			aircraft = this.repository.findAircraftById(aircraftId);
+			status = aircraftId == 0 || aircraft != null && super.getRequest().getPrincipal().hasRealmOfType(Technician.class);
 		}
 
 		super.getResponse().setAuthorised(status);
